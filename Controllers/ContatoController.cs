0146536e0d22dc10c97d1cbac59cf68d21367b6d
Evandro Petrus <evandro_petrus@outlook.com>
2022-09-16 01:06:02 -0300

    HTML page made + connected to DB + added methods

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
+using MVC02.Context;
 namespace MVC02.Controllers
 {
     public class ContatoController : Controller
     {
+
+        private readonly AgendaContext _context;
+
+        public ContatoController(AgendaContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var contatos = _context.Contatos.ToList();
+            return View(contatos);
         }
     }
 }
